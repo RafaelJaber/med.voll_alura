38b7b1117e856f9b7eb3ce90f76bc43ecc6ad795
Rafael Jáber <rafael.jaber@hotmail.com>
2023-05-04 00:37:00 -0300

    Implementada a classe JPA e o Repositorio DoctorRepository.java para realizar as operações com o banco.

---
+++ src/main/java/med/voll/api/model/Address.java
@@ -0,0 +1,20 @@
+package med.voll.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String publicPlace;
+    private String neighborhood;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String complement;
+    private String number;
+}
