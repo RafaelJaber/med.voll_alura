94e3e177564c74de794974f283f31f8174356bde
Rafael Jáber <rafael.jaber@hotmail.com>
2023-05-05 19:50:56 -0300

    Criada a tabela de usuário e suas entidades, foi configurado também o comportamento padrão do spring.

---
+++ src/main/java/med/voll/api/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.user.repository;
+
+import med.voll.api.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+}
