6e470de1b4ce765f51b77936af6e97995716172e
Rafael Jáber <rafael.jaber@hotmail.com>
2023-05-05 22:05:59 -0300

    Confirmando que o usuário está logado para o spring

--- src/main/java/med/voll/api/infrastructure/security/filter/SecurityFilter.java
+++ src/main/java/med/voll/api/infrastructure/security/filter/SecurityFilter.java
@@ -4,8 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.user.repository.UserRepository;
 import med.voll.api.infrastructure.security.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,24 +21,33 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+    @Autowired
+    private UserRepository repository;
 
     @Override
     protected void doFilterInternal(
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
     ) throws ServletException, IOException {
         String tokenJWT = getToken(request);
-        String subject = tokenService.getSubject(tokenJWT);
 
+        if (tokenJWT != null) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails user = repository.findByLogin(subject);
 
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String getToken(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-            throw  new RuntimeException("JWT token not sent in Authorization header!");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
